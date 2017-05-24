81f01ff57c8a65f110dfc7ab22be1be1e2bc14a3
cola_huang <cola_huang@asus.com>
2017-05-24 11:43:04 +0800

    pmic: enable LDO2 vcc33_mipi at bootup
    power up camera module to prevent i2c-2 SDA pulled low
    
    Change-Id: I53b5103ae5ea8afef51a7a909e5f71c00ebb6ca5

--- board/rockchip/miniarm_rk3288/miniarm-rk3288.c
+++ board/rockchip/miniarm_rk3288/miniarm-rk3288.c
@@ -5,3 +5,39 @@
  */
 
 #include <common.h>
+#include <power/regulator.h>
+
+#ifdef CONFIG_DM_PMIC
+static int rockchip_set_regulator_on(const char *name, uint uv)
+{
+	struct udevice *dev;
+	int ret;
+
+	ret = regulator_get_by_platname(name, &dev);
+	if (ret) {
+		debug("%s: Cannot find regulator %s\n", __func__, name);
+		return ret;
+	}
+	ret = regulator_set_value(dev, uv);
+	if (ret) {
+		debug("%s: Cannot set regulator %s\n", __func__, name);
+		return ret;
+	}
+	ret = regulator_set_enable(dev, 1);
+	if (ret) {
+		debug("%s: Cannot enable regulator %s\n", __func__, name);
+		return ret;
+	}
+
+	return 0;
+}
+
+int power_init_board(void)
+{
+	int ret = rockchip_set_regulator_on("vcc33_mipi", 3300000);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+#endif
