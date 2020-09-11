5a0bcbf0fc30d8fc4a0c51d091b6d334cbeaa057
Leslie Yu <Leslie_Yu@asus.com>
2020-09-11 11:23:28 +0800

    Revert "pmic: enable LDO2 vcc33_mipi at bootup"
    This reverts commit dbcc1f115b96ddccff3fe0e7c882e023125a1e29.
    
    Change-Id: Id128decf5b84229a34486da712ccb68f5e8f4392

--- board/rockchip/tinker_rk3288/tinker-rk3288.c
+++ board/rockchip/tinker_rk3288/tinker-rk3288.c
@@ -8,8 +8,6 @@
 #include <dm.h>
 #include <i2c_eeprom.h>
 #include <netdev.h>
-#include <power/regulator.h>
-
 
 static int get_ethaddr_from_eeprom(u8 *addr)
 {
@@ -35,39 +33,3 @@ int rk3288_board_late_init(void)
 
 	return 0;
 }
-
-
-#ifdef CONFIG_DM_PMIC
-static int rockchip_set_regulator_on(const char *name, uint uv)
-{
-	struct udevice *dev;
-	int ret;
-
-	ret = regulator_get_by_platname(name, &dev);
-	if (ret) {
-		debug("%s: Cannot find regulator %s\n", __func__, name);
-		return ret;
-	}
-	ret = regulator_set_value(dev, uv);
-	if (ret) {
-		debug("%s: Cannot set regulator %s\n", __func__, name);
-		return ret;
-	}
-	ret = regulator_set_enable(dev, 1);
-	if (ret) {
-		debug("%s: Cannot enable regulator %s\n", __func__, name);
-		return ret;
-	}
-
-	return 0;
-}
-
-int power_init_board(void)
-{
-	int ret = rockchip_set_regulator_on("vcc33_mipi", 3300000);
-	if (ret)
-		return ret;
-
-	return 0;
-}
-#endif
